b393f7b328e702c3784cba843e29ae163f7ba7ee
dyingg <inktechser@gmail.com>
2019-02-08 00:50:34 +0530

    build and repo creation

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # js-drop-file
-Drag and drop files to JS memory
+
+Easy and simple drag and drop files on the browser
+
+## Documentation
+
+The DropFile class is exposed publicly by the script.
+
+```js
+//Start by creating instance of drop file.
+var fileHandler = new DropFile();
+
+// Alternatively you can constraint the file extension by filtering using
+
+var fileHandler = new DropFile({
+  fileType: "json"
+});
+
+//The above created fileHandler instance will discord all files that do not have the extension .json
+//File type by reading blob filter planned
+```
+
+### onFile
+
+Called when a file has been dropped and consumed, it recieves raw UTF-8 encoded string for the file.
+(Binary support planned)
+
+```js
+fileHandler.onFile(data => {
+  console.log(data);
+
+  var data = JSON.parse(data);
+
+  //Do what you have to with the data file.
+});
+```
+
+### onError
+
+Called when an error occurs such as file type upload does not match filter, dragged element is not a file etc
+
+```js
+fileHandler.onError(msg => {});
+```
+
+Here the returned message is one of the following strings
+
+    READ_ERROR  -> Dragged element was a file of the correct extension but read was interrupted
+    FILE_FILTER_ERROR -> Dragged element was a file but the extension matching failed
+    FILE_FAIL  -> Dragged element was not a file
+
+#### implementation notes (ignore)
+
+- Drag required both ondrop and ondragover to be defined.
---
+++ src/index.js
@@ -0,0 +1,88 @@
+class DropFile {
+  /**
+   * @param {String} [id]
+   * @param  {Object} [options]
+   * @param  {String} [options.fileType]
+   *
+   */
+  constructor(id, options) {
+    if (typeof id !== "string") {
+      throw "Invalid value for field ID";
+    }
+
+    var element = document.getElementById(id);
+    if (!element) {
+      throw "No element found in document with given ID " + id;
+    }
+
+    element.ondrop = this.handle.bind(this);
+    element.ondragover = this.dragover.bind(this);
+
+    this.READ_ERROR = "READ_ERROR";
+    this.FILE_FILTER_ERROR = "FILE_FILTER_ERROR";
+    this.FILE_FAIL = "FILE_FAIL";
+
+    console.log("Added drop handler.");
+
+    this.handleFile == undefined;
+    //Create the file reader
+    this.fileReader = new FileReader();
+
+    this.fileReader.onloadend = function(ev) {
+      if (ev.target.result) {
+        if (this.handleFile !== undefined) {
+          //Takes raw utf-8 string data
+          if (!ev.target.error) {
+            this.handleFile(ev.target.result);
+          } else {
+            this.errorHandler(this.READ_ERROR);
+          }
+        }
+      } else {
+        this.errorHandler(this.READ_ERROR);
+      }
+    }.bind(this);
+
+    //Set the file type constraint
+    this.fileType = options.fileType || "ANY_FILE";
+  }
+
+  handle(ev) {
+    ev.preventDefault();
+    if (ev.dataTransfer.files.length > 0) {
+      var currentFile = ev.dataTransfer.files[0];
+
+      if (
+        this.fileType == "ANY_FILE" ||
+        currentFile.name.split(".").pop() == this.fileType
+      ) {
+        this.fileReader.readAsText(currentFile);
+      } else {
+        console.log(this.errorHandler);
+        this.errorHandler(this.FILE_FILTER_ERROR);
+        console.log("Invalid file type");
+      }
+    } else {
+      this.errorHandler(this.FILE_FAIL);
+    }
+  }
+
+  onFile(cb) {
+    //Set handle File to handle reading of files.
+    this.handleFile = cb;
+  }
+
+  onError(cb) {
+    //Save the error hanndler
+    this.errorHandler = cb;
+  }
+
+  //Better if we abstract this out of the way.
+  dragover(ev) {
+    ev.preventDefault();
+
+    //console.log(ev);
+  }
+}
+
+window.DropFile = DropFile;
